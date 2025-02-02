47bdd126c4e6dafc439b832465a69148b3959644
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-02 19:32:23 +0700

    server run

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8989", nil)
+
+}
